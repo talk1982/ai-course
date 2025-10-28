98059b1990974eced88c395e612fc2f87445ec72
talk1982 <71708071+talk1982@users.noreply.github.com>
2025-10-28 21:56:13 +0200

    Add exercises

--- class_01/class_03/class_03.py
+++ class_01/class_03/class_03.py
@@ -91,3 +91,82 @@ print(data)
 squards = [i**2 for i in range(10) if i%2 == 0]
 print(squards)
 # %%
+f = lambda x: 2*x+1
+f(5)
+# %%
+def cube(x):
+    return x*x*x
+
+list(map(cube, range(10)))
+#like for 
+# %%
+
+def filter_vowels(variable):
+    letters = ['a']
+    if(variable in letters):
+        return True
+    else:
+        return False
+
+sequence = "encyclopedia"
+list(filter(filter_vowels, sequence))
+# %%
+
+from functools import reduce
+lst = [1,2,3,4]
+
+product = reduce((lambda x, y: x*y),lst)
+
+print(product)
+# %%
+books = [ 
+    {"title": "Python 101", "price": 39.99, "rating": 4.5}, 
+    {"title": "Deep Learning", "price": 59.99, "rating": 4.8}, 
+    {"title": "AI for Beginners", "price": 29.99, "rating": 4.2}, 
+    {"title": "Data Science Daily", "price": 49.99, "rating": 3.9}, 
+    {"title": "The Bug Book", "price": 19.99, "rating": 3.2} ]
+
+def is_highly_rated(rating):
+    if(rating >= 4.5):
+        return True
+    return False
+
+for book in books:
+    print("Book:") 
+    for key, value in book.items(): 
+        print(f" {key}: {value}" )
+        if(key == "rating"):
+            print(f" Is highly rated? -> {is_highly_rated(book["rating"])}")
+
+
+
+def get_top_books(book_list):
+    "# Write a function get_top_books(book_list) that uses the first function to filter top-rated books."
+    finalList = []
+    for book in book_list:
+        for bookKeys in book:
+            if(bookKeys == "rating"):
+                if(is_highly_rated(book[bookKeys])):
+                    finalList.append(book)
+    return finalList
+
+print(get_top_books(books))
+
+#Use the sorted() function to sort Books by rating (high to low) and print the top 3 results.
+def get_key(book):
+    for key, value in book.items():
+        if key == "price":
+            return value
+
+ 
+print(f"{sorted(books, key = get_key, reverse=True)}")
+
+#Magic functions: Use map() with a lambda to round all book prices to the nearest integer.
+rounded_prices = list(map(lambda dic_book: round(dic_book["price"]), books))
+print(f" --- {rounded_prices}")
+#Use filter() to keep books under $40
+
+adorable_book = list(filter(lambda x: x["price"]<40, books))
+print(adorable_book)
+print([book["title"] for book in adorable_book])
+# %%
